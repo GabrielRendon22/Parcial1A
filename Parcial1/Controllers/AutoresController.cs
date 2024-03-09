ba3894d00a57d0fbd057baabfb33ecf1c03d760d
arrevillagamario <marrevillaga03@gmail.com>
2024-03-09 12:52:07 -0600

    Conexion exitosa

---
+++ Parcial1/Controllers/AutoresController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial1.Models;
+
+namespace Parcial1.Controllers
+{
+    public class AutoresController : Controller
+    {
+        public Parcial1aContext _parcial1AContext1;
+        public AutoresController(Parcial1aContext parcial1AContext)
+        {
+            _parcial1AContext1 = parcial1AContext;
+        }
+
+        [HttpGet]
+        [Route("autores")]
+        public IActionResult Index()
+        {
+            List<Autore> autores = (from e in _parcial1AContext1.Autores select e).ToList();
+
+            if (autores.Count()==0)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                return Ok(autores);
+            }
+            
+        }
+    }
+}
